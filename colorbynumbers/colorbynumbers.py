2bcc9f22c89714249a070e05de2200242444bad9
Peter Rennert <p.rennert@cs.ucl.ac.uk>
2016-01-02 20:39:57 +0000

    correct type for subplot

--- colorbynumbers/colorbynumbers.py
+++ colorbynumbers/colorbynumbers.py
@@ -157,7 +157,7 @@ def plot_palette(dom_colors):
     w = 5.0
     h = np.ceil(l / w)
 
-    axes = fig.add_subplot(h, w)
+    axes = fig.add_subplot(int(h), int(w))
 
     for i, c in enumerate(dom_colors):
         ax = axes[i]
